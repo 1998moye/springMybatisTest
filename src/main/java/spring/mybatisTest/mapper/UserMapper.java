0400deffab2273c0be0afabb3cd2b66ec3e3d5a6
qpone <1806314252@qq.com>
2021-08-05 15:38:03 +0800

    'mapper'

--- src/main/java/spring/mybatisTest/mapper/UserMapper.java
+++ src/main/java/spring/mybatisTest/mapper/UserMapper.java
@@ -1,4 +1,10 @@
 package spring.mybatisTest.mapper;
 
-public class UserMapper {
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import spring.mybatisTest.entity.User;
+
+public interface UserMapper {
+    @Select("select * from m_user where id = #{id}")
+    public User getUser(@Param("id")Long id);
 }
